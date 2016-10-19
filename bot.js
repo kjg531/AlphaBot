70c6913cc4677852a824be6e166674d11489794e
aaj <mando.alvarado.jose@gmail.com>
2016-10-18 20:05:10 -0600

    Fixed status cycling (was too frequent)

--- bot.js
+++ bot.js
@@ -8178,7 +8178,7 @@ function addExtension(extension, svr, consoleid, callback) {
 // Default game: rotates between stats
 function defaultGame(i, force) {
     // var games = [bot.servers.length + " server" + (bot.servers.length==1 ? "" : "s") + " connected", "serving " + bot.users.length + " users", "redditalpha.com", "v" + version, "Twitter @RedditAlpha", configs.hosting || "limited mode", "Reddit Alpha Clan Family"];
-    var games = ['Reddit', 'Alpha', 'Clan', 'Family']
+    var games = ['Reddit Alpha Clan Family', 'Twitter @RedditAlpha'];
     if(configs.game=="default" || force) {
         if(i>=games.length) {
             i = 0;
@@ -8187,7 +8187,7 @@ function defaultGame(i, force) {
             bot.setStatus("online", games[i]);
             setTimeout(function() {
                 defaultGame(++i);
-            }, 2000);
+            }, 15000);
         } catch(err) {
             ;
         }
